086d5c162d106c4d7416ab4661b4c56d5758e980
Godfrey Ndungu Mac <godfreyndungumac@Godfreys-Macbook-Pro.local>
2024-10-26 23:41:48 +0300

    ✨ feat: add Celery Beat scheduler and spider invocation setup
    Configured Celery Beat to schedule periodic tasks for currency data collection
    - Added signal handlers to set up scheduled tasks upon app initialization
    - Created task functions to invoke spiders for NCBA

--- aggregator/apps.py
+++ aggregator/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AggregatorConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "aggregator"
+
+    def ready(self):
+        import aggregator.receivers # noqa
---
+++ aggregator/receivers.py
@@ -0,0 +1,26 @@
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
+
+
+@receiver(post_migrate)
+def setup_periodic_tasks(sender, **kwargs):
+    print("-------------------------------------------------------------")
+    print("-------------------------------------------------------------")
+    print("-------------------------------------------------------------")
+    print("-------------------------------------------------------------")
+    print("-------------------------------------------------------------")
+    print("-------------------------------------------------------------")
+    print("-------------------------------------------------------------")
+    print("-------------------------------------------------------------")
+    print("-------------------------------------------------------------")
+    schedule, created = IntervalSchedule.objects.get_or_create(
+        every=1,
+        period=IntervalSchedule.MINUTES,
+    )
+
+    PeriodicTask.objects.update_or_create(
+        interval=schedule,
+        name="Collect NCBA Bank Data",
+        task="aggregator.tasks.collect_data_ncba",
+    )
